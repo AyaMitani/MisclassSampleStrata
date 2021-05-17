f31f69b36e3341a8661b620c87c895a55f83431f
Aya Mitani <ayamitani@gmail.com>
2021-05-17 14:59:39 -0400

    Update Create_Table_4_and_Table_SensAnal.R

--- Create_Table_4_and_Table_SensAnal.R
+++ Create_Table_4_and_Table_SensAnal.R
@@ -9,6 +9,7 @@ library(ggplot2)
 library(ggthemes)
 library(xtable)
 library(margins)
+library(here)
 
 
 #---------------------------
@@ -39,11 +40,12 @@ ORtable <- function(sumdesignagnostic, summodelbased, sumdesignbased, variable, 
   print.xtable(ORtable, include.rownames = FALSE)
 }
 
+
 ### design-based with truncated weights
 
 designbased_tr <- function(cutoff){
-  analdata$svywt_tr <- ifelse(analdata$svywt > cutoff, cutoff, analdata$svywt)
-  design_tr <- svydesign(id = ~patient.id, strata = ~ e.race.eth.5 + e.agelt35 + e.female + e.rural + e.edugrp, weights = ~svywt_tr, data = analdata)
+  findata$svywt_tr <- ifelse(findata$svywt > cutoff, cutoff, findata$svywt)
+  design_tr <- svydesign(id = ~patient.id, strata = ~ e.race.eth.5 + e.agelt35 + e.female + e.rural + e.edugrp, weights = ~svywt_tr, data = findata)
   designbased_tr <- svyglm(s.trust ~ as.factor(s.race.eth.5) + s.poverty + s.agelt35 + as.factor(s.female) + s.rural + s.edu_L + s.edu_M, design = design_tr, family = quasibinomial("logit") )
   sumdesignbased_tr <- summary(designbased_tr)
 }
@@ -57,8 +59,8 @@ designbased_tr <- function(cutoff){
 # Load analytical data set
 #------------------------------
 
-setwd("") ### ENTER PATH HERE
-load("analdata.Rdata")
+
+findata <- read.table(here("findata.txt"), header = TRUE)
 
 
 
@@ -69,11 +71,11 @@ load("analdata.Rdata")
 #------------------------------
 
 ### design agnostic
-designagnostic <- glm(s.trust ~ as.factor(s.race.eth.5) + s.poverty + s.agelt35 + as.factor(s.female) + s.rural + s.edu_L + s.edu_M, data = analdata, family = binomial("logit"))
+designagnostic <- glm(s.trust ~ as.factor(s.race.eth.5) + s.poverty + s.agelt35 + s.female + s.rural + s.edu_L + s.edu_M, data = findata, family = binomial("logit"))
 sumdesignagnostic <- summary(designagnostic)
 
 ### model-based (adjusting for stratification variables)
-modelbased <- glm(s.trust ~ as.factor(s.race.eth.5) + s.poverty + s.agelt35 + as.factor(s.female) + s.rural + s.edu_L + s.edu_M + as.factor(e.race.eth.5) + e.agelt35 + e.female + e.rural + as.factor(e.edugrp), data = analdata, family = binomial("logit"))
+modelbased <- glm(s.trust ~ as.factor(s.race.eth.5) + s.poverty + s.agelt35 + s.female + s.rural + s.edu_L + s.edu_M + as.factor(e.race.eth.5) + e.agelt35 + e.female + e.rural + as.factor(e.edugrp), data = findata, family = binomial("logit"))
 summodelbased <- summary(modelbased)
 # Calculate average marginal effect
 modelbased_marg = margins(modelbased)
@@ -81,15 +83,15 @@ modelbased_marg = margins(modelbased)
 summary(modelbased_marg)
 
 ### design-based (weighted analysis)
-design <- svydesign(id = ~patient.id, strata = ~ e.race.eth.5 + e.agelt35 + e.female + e.rural + e.edugrp, weights = ~svywt, data = analdata)
-designbased <- svyglm(s.trust ~ as.factor(s.race.eth.5) + s.poverty + s.agelt35 + as.factor(s.female) + s.rural + s.edu_L + s.edu_M, design = design, family = quasibinomial("logit") )
+design <- svydesign(id = ~patient.id, strata = ~ e.race.eth.5 + e.agelt35 + e.female + e.rural + e.edugrp, weights = ~svywt, data = findata)
+designbased <- svyglm(s.trust ~ as.factor(s.race.eth.5) + s.poverty + s.agelt35 + s.female + s.rural + s.edu_L + s.edu_M, design = design, family = quasibinomial("logit") )
 sumdesignbased <- summary(designbased)
 
 ### design-based (weighted analysis) with weights truncated at 90th percentile
-cutoff <- quantile(analdata$svywt, 0.90)
-analdata$svywt_tr <- ifelse(analdata$svywt > cutoff, cutoff, analdata$svywt)
-design_tr <- svydesign(id = ~patient.id, strata = ~ e.race.eth.5 + e.agelt35 + e.female + e.rural + e.edugrp, weights = ~svywt_tr, data = analdata)
-designbased_tr <- svyglm(s.trust ~ as.factor(s.race.eth.5) + s.poverty + s.agelt35 + as.factor(s.female) + s.rural + s.edu_L + s.edu_M, design = design_tr, family = quasibinomial("logit") )
+cutoff <- quantile(findata$svywt, 0.90)
+findata$svywt_tr <- ifelse(findata$svywt > cutoff, cutoff, findata$svywt)
+design_tr <- svydesign(id = ~patient.id, strata = ~ e.race.eth.5 + e.agelt35 + e.female + e.rural + e.edugrp, weights = ~svywt_tr, data = findata)
+designbased_tr <- svyglm(s.trust ~ as.factor(s.race.eth.5) + s.poverty + s.agelt35 + s.female + s.rural + s.edu_L + s.edu_M, design = design_tr, family = quasibinomial("logit") )
 sumdesignbased_tr <- summary(designbased_tr)
 
 
@@ -109,10 +111,15 @@ ORtable(sumdesignagnostic, summodelbased, sumdesignbased_tr,  variable, p=11)
 #--------------------------------------------------------------------------
 
 
-sumdesignbased_tr100 <- designbased_tr(quantile(analdata$svywt, 1))
-sumdesignbased_tr99 <- designbased_tr(quantile(analdata$svywt, 0.99))
-sumdesignbased_tr95 <- designbased_tr(quantile(analdata$svywt, 0.95))
+sumdesignbased_tr100 <- designbased_tr(quantile(findata$svywt, 1))
+sumdesignbased_tr99 <- designbased_tr(quantile(findata$svywt, 0.99))
+sumdesignbased_tr95 <- designbased_tr(quantile(findata$svywt, 0.95))
 
 variable <- c("Intercept", "Black", "Asian", "Other", "Hispanic", "Poverty", "Age $lt$ 35", "Female", "Rural", "Less than HS", "Some college")
 ORtable(sumdesignbased_tr100, sumdesignbased_tr99, sumdesignbased_tr95,  variable, p=11)
+
+
+
+
+
 
